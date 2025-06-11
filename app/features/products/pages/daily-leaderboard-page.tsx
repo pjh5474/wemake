225459433e64be53bf8470f4517cc10617ab50af
pjh5474 <warwarsn@gmail.com>
2025-06-11 18:05:38 +0900

    Data validation

--- app/features/products/pages/daily-leaderboard-page.tsx
+++ app/features/products/pages/daily-leaderboard-page.tsx
@@ -1,5 +1,13 @@
-import type { Route } from "~/types";
-import { type MetaFunction } from "react-router";
+import { data, isRouteErrorResponse, type MetaFunction } from "react-router";
+import type { Route } from "./+types/daily-leaderboard-page";
+import { DateTime } from "luxon";
+import { z } from "zod";
+
+const paramsSchema = z.object({
+  year: z.coerce.number().int(),
+  month: z.coerce.number().int(),
+  day: z.coerce.number().int(),
+});
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,38 +19,63 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export function loader({ request, params }: Route.LoaderArgs) {
-  const { year, month, day } = params;
-  return { year, month, day };
-}
-
-export function action({ request }: Route.ActionArgs) {
-  return {};
-}
+export const loader = ({ request, params }: Route.LoaderArgs) => {
+  const { success, data: parsedData } = paramsSchema.safeParse(params);
+  if (!success) {
+    throw data(
+      {
+        error_code: "INVALID_PARAMS",
+        message: "Invalid params",
+      },
+      { status: 400 }
+    );
+  }
+  const date = DateTime.fromObject(parsedData).setZone("Asia/Seoul");
+  if (!date.isValid) {
+    throw data(
+      {
+        error_code: "INVALID_DATE",
+        message: "Invalid date",
+      },
+      { status: 400 }
+    );
+  }
+  const today = DateTime.now().setZone("Asia/Seoul").startOf("day");
+  if (date > today) {
+    throw data(
+      {
+        error_code: "FUTURE_DATE",
+        message: "Future date",
+      },
+      { status: 400 }
+    );
+  }
+  return {
+    date,
+  };
+};
 
 export default function DailyLeaderboardPage({
   loaderData,
-  actionData,
-}: Route.ComponentProps<{ year: string; month: string; day: string }>) {
-  const date = new Date(
-    parseInt(loaderData.year),
-    parseInt(loaderData.month) - 1,
-    parseInt(loaderData.day)
-  );
-
+}: Route.ComponentProps) {
   return (
     <div className="container py-8">
-      <h1 className="text-4xl font-bold">
-        {date.toLocaleDateString("en-US", {
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        })}{" "}
-        Leaderboard
-      </h1>
-      <div className="mt-8">
-        {/* Leaderboard content will be rendered here */}
-      </div>
+      <h1 className="text-4xl font-bold">Leaderboard</h1>
+      <div className="mt-8"></div>
     </div>
   );
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        {error.data.message} / {error.data.error_code}
+      </div>
+    );
+  }
+  if (error instanceof Error) {
+    return <div>{error.message}</div>;
+  }
+  return <div>Unknown error</div>;
 }
